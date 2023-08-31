dada1a8199824db50508340a7fe82d02f0949bdb
Veerabommu Nikhil <131293965+Veerabommunikhil@users.noreply.github.com>
2023-08-31 23:44:38 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,8 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println(MESSAGE);
+        System.out.println(MESSAGE); 
+        
     }
 
     public String getMessage() {
